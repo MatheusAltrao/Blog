ba819e1e331c8881e2e09f17662ec58c1c0f152e
Matheus Altrão <matheusaltrao2@gmail.com>
2024-01-17 01:00:40 -0400

    feat: add spinner

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,9 +2,10 @@ import Image from 'next/image';
 import GoogleIcon from '../assets/google.svg';
 import { signIn, useSession, signOut } from 'next-auth/react';
 import { X } from 'lucide-react';
+import Spinner from './Spinner';
 
 const Header = () => {
-    const { data: session } = useSession();
+    const { data, status } = useSession();
 
     return (
         <header className='flex items-center justify-between py-4 w-full border-b border-zinc-900  '>
@@ -12,17 +13,22 @@ const Header = () => {
                 <Image width={40} height={40} src='/favicon.ico' alt='' />
             </div>
 
-            {session && (
+            {status === 'authenticated' && (
                 <button
                     onClick={() => signOut()}
                     className='flex items-center justify-center gap-2 border border-zinc-800 hover:bg-transparent transition-colors  bg-zinc-800 rounded py-2 px-4 overflow-hidden'
                 >
-                    {session.user?.name}{' '}
-                    <X className='text-zinc-500' size={18} />
+                    {data.user?.name} <X className='text-zinc-500' size={18} />
                 </button>
             )}
 
-            {!session && (
+            {status === 'loading' && (
+                <button className='flex items-center justify-center gap-2 border border-zinc-800 hover:bg-transparent transition-colors  bg-zinc-800 rounded py-2 w-[100px] overflow-hidden'>
+                    <Spinner />
+                </button>
+            )}
+
+            {status === 'unauthenticated' && (
                 <button
                     onClick={() => signIn('google')}
                     className='flex items-center justify-center gap-2 border border-zinc-800 hover:bg-transparent transition-colors  bg-zinc-800 rounded py-2 px-4 overflow-hidden'
